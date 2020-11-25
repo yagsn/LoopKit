86c3848f71cf09697809857af901f56db19d1725
Pete Schwamb <pete@schwamb.net>
2020-11-25 10:03:56 -0600

    Export LoopKitUI

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LoopKit",
-            targets: ["LoopKit"]),
+            targets: ["LoopKit", "LoopKitUI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ps2/SwiftCharts.git", .branch("uikit-explicit"))
