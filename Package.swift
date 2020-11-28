324f2641cb7b49569564ef35e64e98967234772b
Pete Schwamb <pete@schwamb.net>
2020-11-28 09:27:27 -0600

    Add watchOS support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LoopKit",
     defaultLocalization: "en",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .watchOS(.v4)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "LoopKit", type: .dynamic, targets: ["LoopKit"]),
